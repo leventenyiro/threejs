5775f1393065ee194870868033b2db07942cad5d
leventenyiro <nyiro.levente@gmail.com>
2024-03-21 11:41:40 +0100

    modify background

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 self.onmessage = function(event) {
     var messageData = event.data;
     
-    console.log('Received message in background script:', messageData);
+    console.log('Received message in background: ', messageData);
 
     self.postMessage('Message received by background script');
 };
